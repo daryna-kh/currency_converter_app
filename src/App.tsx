32aa51b88425aa38585d3009fd0dbd017734522b
Daryna Bilan <rinabkh98@gmail.com>
2024-02-28 12:19:24 +0200

    add test fetch request #main

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const originUrl = `http://api.nbp.pl/api/`;
+
+function getValues() {
+  try {
+    const response = fetch(`http://api.nbp.pl/api/exchangerates/rates/a/chf/`, {
+      method: "GET",
+      headers: {
+        Accept: "*/*",
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => getValues}>Hi HU HU</button>
     </div>
   );
 }
